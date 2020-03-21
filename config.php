384838fb94f938e5b9d741a2c9a0c654248a496d
Stefan.Stafie <stefan_stafie@yahoo.com>
2020-03-21 11:23:42 +0200

    fb Test Login succes. Aquired access_token

---
+++ config.php
@@ -0,0 +1,7 @@
+
+<?php
+//header cu chestiile principale legate de aplicatie creata pe facebook developers
+	define('MY_FB_APP_ID','26211813147682711400941363412130');
+	define('MY_FB_APP_SECRET','f3ccb98dc558350d9dc80914ac413655');
+	define('INSTAGRAM_APP_REDIRECT_URI','https://localhost/IG%20Basic%20Display%20Test/'); //pagina pe care sa fiu redirectionat dupa ce ma loghez cu instagram
+?>
