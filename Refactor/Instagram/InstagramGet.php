a3714e03474ac93b97a136af90e1874a36b97a25
amaterasuonpc <stefanica.catalin@gmail.com>
2020-04-10 17:35:21 +0300

    Refactor for insta

--- Refactor/Instagram/InstagramGet.php
+++ Refactor/Instagram/InstagramGet.php
@@ -1,11 +1,19 @@
 <?php
 
-require '../InstagramUser.php'
+require './InstagramUser.php';
 
 class InstagramGet extends InstagramUser {
     public function getPost($postId){
-        $post=this->fb->get($postId . '?fields=caption,comment_count,like_count,media_type,media_url');
+        try {
+        $post=$this->fb->get($postId . '?fields=caption,comment_count,like_count,media_type,media_url');
         return $post;
+        }catch (Facebook\Exceptions\FacebookResponseException $e) {
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
 
     }
 };
@@ -14,6 +22,6 @@ class InstagramGet extends InstagramUser {
 $Idinsta='17841403789483121';
 
 $test = new InstagramGet('EAAQM6NoB0t0BAD6fyvsOyKqAbdMijXAhPiLe7wQZBp4KrFV1f08LTYNptC4Wa6Yqw8ZAJLHlbIl5SpxhsTeab8KJkIlX5jwJ1f6DJ9Bu3ZBhuAbQHwpCPZA3G4ZBJKDzBxZAKhGypAvAbXTe23vs182Yrw1AZBZBguVfgdR46lJ8IFiI6qvVtQe3AfMusXZBK3z4ZD');
-
-echo $test ;
+$test2=$test->getPost(17850222661964436);
+echo $test2;
 
--- Refactor/Instagram/InstagramUser.php
+++ Refactor/Instagram/InstagramUser.php
@@ -1,18 +1,24 @@
 <?php
 
-require_once 'vendor/php-graph-sdk/autoload.php';
+require_once './vendor/php-graph-sdk/autoload.php';
 
 class InstagramUser {
-    public $accesToken
-    public $fb
+    public $accesToken;
+    public $fb;
     public $appCreds = array( // array to hold app creds from fb app
 		'app_id' => '1140094136341213',
 		'app_secret' => 'f3ccb98dc558350d9dc80914ac413655',
 		'default_graph_version' => 'v6.0'
 	);
     function __construct($accessToken) {
-        this->$appCreds$appCreds['default_access_token'] = $accesToken;
-		$this->fb = new Facebook\Facebook($appCreds);
+         $this->$appCreds = array( // array to hold app creds from fb app
+            'app_id' => '1140094136341213',
+            'app_secret' => 'f3ccb98dc558350d9dc80914ac413655',
+            'default_graph_version' => 'v6.0'
+        );
+        $this->$appCreds['default_access_token'] = $accessToken;
+        echo $appCreds;
+		$this->fb = new Facebook\Facebook($this->$appCreds);
 		$this->accessToken = $accessToken;
     }
 
