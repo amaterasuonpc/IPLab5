6a3c09e139b3e054b4f7b0db8827f678bca6bf0a
amaterasuonpc <stefanica.catalin@gmail.com>
2020-04-12 16:19:10 +0300

    Update InstagramGet.php

--- Refactor/Instagram/InstagramGet.php
+++ Refactor/Instagram/InstagramGet.php
@@ -22,6 +22,6 @@ class InstagramGet extends InstagramUser {
 $Idinsta='17841403789483121';
 
 $test = new InstagramGet('EAAQM6NoB0t0BAD6fyvsOyKqAbdMijXAhPiLe7wQZBp4KrFV1f08LTYNptC4Wa6Yqw8ZAJLHlbIl5SpxhsTeab8KJkIlX5jwJ1f6DJ9Bu3ZBhuAbQHwpCPZA3G4ZBJKDzBxZAKhGypAvAbXTe23vs182Yrw1AZBZBguVfgdR46lJ8IFiI6qvVtQe3AfMusXZBK3z4ZD');
-$test2=$test->getPost(17850222661964436);
+$test2=$test->getPost('17850222661964436');
 echo $test2;
 
