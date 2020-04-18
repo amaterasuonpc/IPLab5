4768b3e908214e7315f38ddc6d6258b1368efa45
Stefan Stafie <stefan_stafie@yahoo.com>
2020-04-18 12:38:48 +0300

    all classes in a file + RestApi in a file

--- Refactor/fb/REST.php
+++ Refactor/fb/REST.php
@@ -1,22 +1,26 @@
 <?php
-require_once("FacebookGet.php");
+require_once("./classes.php");
 function deliver()
 {
 	echo "<pre>";
-	if (isset($_GET) && count($_GET) >= 3) {
-		if (isset($_GET['do']) && isset($_GET['postId']) && isset($_GET['token'])){
+	if (isset($_GET)) {
+		if (isset($_GET['do']) && isset($_GET['token'])){
 			$x = new FacebookGet($_GET['token']);
-			$post = new Post($x->getWholePost($_GET['postId']));
+			
 			switch ($_GET['do']) {
+				//for GET requests DOWN-------------------------------------------------------------
 				case 'getComments':
+					$post = new Post($x->getWholePost($_GET['postId']));
 					print_r($post->getComments());
 					break;
 
 				case 'getCommentCount':
+					$post = new Post($x->getWholePost($_GET['postId']));
 					print_r($post->getCommentCount());
 					break;
 
 				case 'getLikeCount':
+					$post = new Post($x->getWholePost($_GET['postId']));
 					print_r($post->getLikeCount());
 					break;
 
@@ -27,8 +31,31 @@ function deliver()
 				case 'getPostsArray':
 					print_r($x->getPostsArray($_GET['userId']));
 					break;
-				
-			
+				case 'last3comments':
+					print_r($x->last3Comments($_GET['postId']));
+					break;
+				case 'getBestPost':
+					print_r($x->getBestPost($_GET['userId']));
+					break;
+				//for POST requests DOWN--------------------------------------------------------
+				case 'PostUrl':
+					$obiect = new FacebookPost($x->accessToken);
+					print_r($obiect->postareUrl($_GET['url'],$_GET['mesaj']));
+					break;
+				case 'PostImage':
+					$obiect = new FacebookPost($x->accessToken);
+					print_r($obiect->postareImage($_GET['url'],$_GET['mesaj']));
+					break;
+				case 'PostMessage':
+					$obiect = new FacebookPost($x->accessToken);
+					print_r($obiect->postareMesaj($_GET['image'],$_GET['mesaj']));
+					break;
+				case 'PostVideo':
+					$obiect = new FacebookPost($x->accessToken);
+					print_r($obiect->postareVideo($_GET['url'],$_GET['mesaj']));
+					break;
+				//for LOGIN
+
 				default:
 					break;
 			}
--- Refactor/fb/requestCreator.php
+++ Refactor/fb/requestCreator.php
@@ -1,17 +1,95 @@
+<style>
+    form {
+        display: inline-block;
+        background-color: whitesmoke;
+        border: 5px solid black;
+        padding: 5px;
+        margin: 2px;
+    }
+</style>
+
+
+<!-- -------------------------GET -------------------------->
 <form action="REST.php" method="get">
-    <select  id="do" name="do">
-            <option value="getComments">comments</option>
-            <option value="getCommentCount">comment count</option>
-            <option value="getLikeCount">like count</option>
-            <option value="getPages">get pages</option>
-            <option value="getPostsArray">get Posts</option>
-            
+    <select id="do" name="do">
+        <option value="getComments">comments</option>
+        <option value="getCommentCount">comment count</option>
+        <option value="getLikeCount">like count</option>
+        <option value="last3comments">last 3 comments</option>
     </select><br>
-    <label>user ID:</label><br>
-    <input type="text" name="userId" /><br>
     <label>post ID:</label><br>
     <input type="text" name="postId" /><br>
     <label>access_token:</label><br>
     <input type="text" name="token" /><br>
     <input type="submit" value="Submit" />
 </form>
+
+<form action="REST.php" method="get">
+    <select id="do" name="do">
+        <option value="getPages">get pages</option>
+    </select><br>
+    <label>USER ID:</label><br>
+    <input type="text" name="userId" /><br>
+    <label>ACCESS TOKEN:</label><br>
+    <input type="text" name="token" /><br>
+    <input type="submit" value="Submit" />
+</form>
+
+<form action="REST.php" method="get">
+    <select id="do" name="do">
+        <option value="getPostsArray">get Posts</option>
+        <option value="getBestPost">get best post</option>
+    </select><br>
+    <label>PAGE ID:</label><br>
+    <input type="text" name="userId" /><br>
+    <label>access_token:</label><br>
+    <input type="text" name="token" /><br>
+    <input type="submit" value="Submit" />
+</form>
+
+
+<!-- -------------------------POST-------------------------->
+<form action="REST.php" method="GET">
+    <select id="do" name="do">
+        <option value="PostUrl">Post URL</option>
+        URL:<input type="text" name="url"><br>
+        Mesaj:<input type="text" name="mesaj"><br>
+        <label>access_token:</label><br>
+        <input type="text" name="token" /><br>
+        <input type="submit" name="submit" value="Url"><br>
+</form>
+
+</form>
+<form action="REST.php" method="GET">
+    <select id="do" name="do">
+        <option value="PostImage">Post Image</option>
+        Image Name:<input type="text" name="image"><br>
+        mesaj:<input type="text" name="mesaj"><br>
+        <label>access_token:</label><br>
+        <input type="text" name="token" /><br>
+        <input type="submit" name="submit" value="Image"><br>
+</form>
+
+</form>
+<form action="REST.php" method="GET">
+    <select id="do" name="do">
+        <option value="PostMessage">Post Message</option>
+        Mesajul dorit:<input type="text" name="messenger"><br>
+        <label>access_token:</label><br>
+        <input type="text" name="token" /><br>
+        <input type="submit" name="submit" value="Message"><br>
+</form>
+
+<form action="REST.php" method="GET">
+    <select id="do" name="do">
+        <option value="PostVideo">Post Video</option>
+        Video Name:<input type="text" name="video"><br>
+        Title:<input type="text" name="title"><br>
+        Description: <input type="text" name="descriere"><br>
+        <label>access_token:</label><br>
+        <input type="text" name="token" /><br>
+        <input type="submit" name="submit" value="Video"><br>
+</form>
+</body>
+
+</html>
